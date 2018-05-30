06f8ae010f3d1f7862584c0a87ed73a1851f5116
Konrad Adamczyk <conrad.adamczyk@gmail.com>
2018-05-31 00:38:42 +0200

    Don't rely on jQuery's deprecated .selector property.

--- adminsortable2/static/adminsortable2/js/inline-stacked.js
+++ adminsortable2/static/adminsortable2/js/inline-stacked.js
@@ -1,11 +1,13 @@
 django.jQuery(function($) {
-	$("script.inline-stacked-config").each(function (i, config_element) {
+	$('script.inline-stacked-config').each(function (i, config_element) {
 		try {
-			var config = JSON.parse(config_element.textContent);
-			$("#" + config["prefix"] + "-group .inline-related").stackedFormset(config);
+			const config = JSON.parse(config_element.textContent);
+			const selector = `#${config.prefix}-group .inline-related`;
+			$(selector).stackedFormset(selector, config);
 		}
 		catch (parse_error) {
-			console.error("Configuration for a django-adminsortable2 inline-stacked form is invalid.");
+			console.error('Configuration for a django-adminsortable2 inline-stacked form is invalid.');
+			console.error(parse_error);
 		}
 	});
 });
--- adminsortable2/static/adminsortable2/js/inline-tabular.js
+++ adminsortable2/static/adminsortable2/js/inline-tabular.js
@@ -1,11 +1,13 @@
 django.jQuery(function($) {
-	$("script.inline-tabular-config").each(function (i, config_element) {
+	$('script.inline-tabular-config').each(function (i, config_element) {
 		try {
-			var config = JSON.parse(config_element.textContent);
-			$("#" + config["prefix"] + "-group .tabular.inline-related tbody tr").tabularFormset(config);
+			const config = JSON.parse(config_element.textContent);
+			const selector = `#${config.prefix}-group .tabular.inline-related tbody tr`;
+			$(selector).tabularFormset(selector, config);
 		}
 		catch (parse_error) {
-			console.error("Configuration for a django-adminsortable2 inline-tabular form is invalid.");
+			console.error('Configuration for a django-adminsortable2 inline-tabular form is invalid.');
+			console.error(parse_error);
 		}
 	});
 });
